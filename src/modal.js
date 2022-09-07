0dea23e8526d417f264bae31ebe1ec23c84f6716
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2022-09-07 17:17:04 +0200

    update

--- src/modal.js
+++ src/modal.js
@@ -13,3 +13,18 @@
     }
 })();
 
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('[buy-open]'),
+        closeModalBtn: document.querySelector('[buy-close]'),
+        modal: document.querySelector('[data-modal]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+})();
+
